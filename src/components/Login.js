0e9c1b2026ad2c876416f83c2902037cba0a0a8a
defney <defney@sabanciuniv.edu>
2020-04-02 17:29:35 +0300

    committed

---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+
+import React, { Component, Fragment } from 'react'
+//import './loginstyle.css';
+
+export default class Login extends React.Component {
+    
+    render() {
+
+  
+        return (
+                <html>
+                    
+                    <body>
+
+                    <h2 style={{color: "blue"}}>&nbsp;LOGIN AS</h2>
+
+                        <form >
+                            &nbsp;&nbsp;<input type="radio" id="user" name="uname" value="user"/>
+                            &nbsp;&nbsp;<label for="male">User</label><br/>
+
+                            &nbsp;&nbsp;<input type="radio" id="admin" name="dname" value="user"/>
+                            &nbsp;&nbsp;<label for="female">Admin</label><br/>
+                        </form>
+
+                        <form  style={{display: 'inline-block'}}>
+                           &nbsp;&nbsp;  <label for="uname">Username:</label><br/>
+                           &nbsp;&nbsp;  <input type="text" id="uname" name="uname" value=" "/><br/>
+
+                           &nbsp;&nbsp;  <label for="password">Password:</label><br/>
+                           &nbsp;&nbsp;  <input type="text" id="password" name="password" value=" "/><br/><br/>
+                           &nbsp;&nbsp;  <input type="submit" value="Login"/><br/><br/><br/>
+                        </form>
+
+
+                        
+
+                        <h2 style={{color: "blue"}}>&nbsp;SIGN UP AS</h2>
+                        <form >
+                            &nbsp;&nbsp;<input type="radio" id="user" name="gender" value="user"/>
+                            &nbsp;&nbsp;<label for="male">User</label><br/>
+                            &nbsp;&nbsp;<input type="radio" id="admin" name="gender" value="user"/>
+                            &nbsp;&nbsp;<label for="female">Admin</label><br/>
+                        </form>
+
+                        <form  style={{display: 'inline-block'}}>
+                            &nbsp;&nbsp;<label for="mail">E-mail address:</label><br/>
+                            &nbsp;&nbsp;<input type="text" id="mail" name="mail" value=" "/><br/>
+
+                            &nbsp;&nbsp;<label for="uname">Username:</label><br/>
+                            &nbsp;&nbsp;<input type="text" id="uname" name="name" value=" "/><br/>
+
+                            &nbsp;&nbsp;<label for="password">Password:</label><br/>
+                            &nbsp;&nbsp;<input type="text" id="password" name="password" value=" "/><br/><br/>
+                            &nbsp;&nbsp;<input type="submit" value="Sign Up"/><br/><br/><br/>
+                        </form>
+
+                       
+
+                    </body>
+                </html>
+                
+
+         )
+    }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React , {Component} from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from "./Button";
+export default class Navbar extends Component {
+    render(){
+        return(
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                {/*https://www.iconfinder.com/icons/1871438/online_shop_shopping_trolley_icon */}
+                <Link to ='./'> 
+                    <img src={logo} alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                    <Link to= "/" className="nav-link">products</Link>
+                    </li>
+                </ul>
+                <Link to = '/cart' className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                        <i className ="fas fa-cart-plus"></i>
+                        </span>
+                            my cart 
+                    </ButtonContainer>
+                    </Link>
+                    {/* login page  button*/}
+                    <Link to = '/login' className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                        <i className ="fas fa-cart-plus"></i>
+                        </span>
+                            Login 
+                    </ButtonContainer>
+                    <ButtonContainer>
+                        <span className="mr-2 align-item-center">
+                        {/* <i className ="fas fa-cart-plus"></i> */}
+                        </span>
+                            Sign Up
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
+        );
+    
+    }
+}
+
+const NavWrapper =styled.nav`
+background : var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform :capitalize ;
+
+}
+`;
