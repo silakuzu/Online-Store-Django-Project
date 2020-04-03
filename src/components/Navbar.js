20887d56349ac1654b3b0f29bac907daa71671ba
zeynepduru <zeynepduru@sabanciuniv.edu>
2020-04-03 16:40:56 +0300

    Navbar.js edited online with Bitbucket zeynepduru: added the logo onto the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react'
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg';
+import logo from '../bringSU-logo.svg';
 import styled from 'styled-components';
 import {ButtonContainer} from "./Button";
 export default class Navbar extends Component {
