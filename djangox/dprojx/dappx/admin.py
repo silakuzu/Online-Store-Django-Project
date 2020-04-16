24d94dc57d0295f3883b32852261b25a48f4daab
defney <defney@sabanciuniv.edu>
2020-04-16 19:39:00 +0300

    djangox added

---
+++ djangox/dprojx/dappx/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+# dappx/admin.py
+
+from dappx.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
--- onlinestorebackend/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Sıla\Desktop\onlinestorebackend\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
