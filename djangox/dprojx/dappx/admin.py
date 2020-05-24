0a01e64bae4342a78b390dd3ad5e2ac597ba7085
defney <defney@sabanciuniv.edu>
2020-05-24 20:42:45 +0300

    correction

--- djangox/dprojx/dappx/admin.py
+++ djangox/dprojx/dappx/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 
 # dappx/admin.py
 
-from dappx.models import UserProfileInfo,productcategories,products
+from dappx.models import UserProfileInfo,productcategories,products,orders,cartItem,cartTable
 # Register your models here.
 admin.site.register(UserProfileInfo)
 admin.site.register(products)
 admin.site.register(productcategories)
+admin.site.register(orders)
+admin.site.register(cartTable)
+admin.site.register(cartItem)
 # admin.site.register(userType)
