62ad9a6a8b2c221ca5ec33591ad44e03e6b651d0
zeynepduru <zeynepduru@sabanciuniv.edu>
2020-05-25 16:50:26 +0300

    admin.py edited online with Bitbucket

--- djangox/dprojx/dappx/admin.py
+++ djangox/dprojx/dappx/admin.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 
 # dappx/admin.py
 
-from dappx.models import UserProfileInfo,productcategories,products,orders,cartItem,cartTable
+from dappx.models import UserProfileInfo,productcategories,products,orders,cartItem,orders
 # Register your models here.
 admin.site.register(UserProfileInfo)
 admin.site.register(products)
 admin.site.register(productcategories)
 admin.site.register(orders)
-admin.site.register(cartTable)
 admin.site.register(cartItem)
 # admin.site.register(userType)
