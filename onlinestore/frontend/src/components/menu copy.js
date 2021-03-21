20a9b5623e7002b81136b1dfaf19c02675fa1a7a
Sila <silakuzu@sabanciuniv.edu>
2021-03-21 23:34:20 +0300

    github

--- onlinestore/frontend/src/components/menu copy.js
+++
@@ -1,8 +0,0 @@
-import TreeMenu from 'react-simple-tree-menu'
-import {treeData} from './menudata';
-
-
-// Use the default minimal UI
-<TreeMenu data={treeData} />
-
-
--- onlinestore/store/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import categoriesViewSet
-
-router = routers.DefaultRouter()
-router.register('api/store',categoriesViewSet,'store')
-
-urlpatterns = router.urls
